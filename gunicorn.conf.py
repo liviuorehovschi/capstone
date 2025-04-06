29365e4e561f4d4aba4d6d1f11b03b823a339ffb
liviuorehovschi <116524714+liviuorehovschi@users.noreply.github.com>
2025-04-06 18:44:20 +0200

    Create gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,13 @@
+# Safer for ML workloads
+workers = 1
+threads = 1
+worker_class = "sync"
+
+# Increase timeout to support model inference
+timeout = 600
+
+# Bind to correct port
+bind = "0.0.0.0:10000"
+
+# Avoid loading model before forking — this is safer for TF/Keras
+preload_app = False
